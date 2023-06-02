589ec9dbfb59dacec994340f645683d32ad0f09d
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2023-06-02 21:12:30 +0530

    Updated App.js with JSON Viewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+
+const JsonViewer = () => {
+  const [jsonContent, setJsonContent] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const content = e.target.result;
+      try {
+        const parsedJson = JSON.parse(content);
+        setJsonContent(JSON.stringify(parsedJson, null, 2));
+      } catch (error) {
+        console.error('Error parsing JSON file:', error);
+        setJsonContent(null);
+      }
+    };
+
+    reader.readAsText(file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>JSON Viewer</h1>
+      <input type="file" onChange={handleFileChange} accept=".json" />
+      {jsonContent && (
+        <pre>{jsonContent}</pre>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default JsonViewer;
